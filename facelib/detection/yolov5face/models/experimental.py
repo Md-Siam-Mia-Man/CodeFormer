d42ae45946cd58d33438d2dd0921de2e10472814
Md Siam Mia <mdsiammia.man@gmail.com>
2025-12-03 09:33:03 +0600

    Update YOLOv5 experimental models with improved formatting and code quality

--- facelib/detection/yolov5face/models/experimental.py
+++ facelib/detection/yolov5face/models/experimental.py
@@ -35,9 +35,16 @@ class MixConv2d(nn.Module):
             a -= np.roll(a, 1, axis=1)
             a *= np.array(k) ** 2
             a[0] = 1
-            c_ = np.linalg.lstsq(a, b, rcond=None)[0].round()  # solve for equal weight indices, ax = b
+            c_ = np.linalg.lstsq(a, b, rcond=None)[
+                0
+            ].round()  # solve for equal weight indices, ax = b
 
-        self.m = nn.ModuleList([nn.Conv2d(c1, int(c_[g]), k[g], s, k[g] // 2, bias=False) for g in range(groups)])
+        self.m = nn.ModuleList(
+            [
+                nn.Conv2d(c1, int(c_[g]), k[g], s, k[g] // 2, bias=False)
+                for g in range(groups)
+            ]
+        )
         self.bn = nn.BatchNorm2d(c2)
         self.act = nn.LeakyReLU(0.1, inplace=True)
 
